8636e8d1c2efc3c30ef09ce9688c6860476f16e4
leliebe <54686254+leliebe@users.noreply.github.com>
2026-04-01 17:35:28 -0400

    Adding Stardew test data, clarifying arrays

--- Catalog (WIP)/catalogData.js
+++ Catalog (WIP)/catalogData.js
@@ -31,12 +31,12 @@ class Item {
     constructor(name, description, category, platfrom, developer, publisher, releaseDate, tags) {
         this.name = name; //This should be a String
         this.description = description; //This should be a String
-        this.category = category;
+        this.category = category; //This should be an Array; see external taxonomy
         this.platform = platfrom; //This should be an Array even if it only contains one item.
         this.developer = developer; //This should be a String
         this.publisher = publisher; //This should be a String
         this.releaseDate = releaseDate; //This should be an Array even if it only contains one item.
-        this.tags = tags;
+        this.tags = tags; //This should be an Array; see external taxonomy
     }
 
     getName() {
@@ -47,7 +47,8 @@ class Item {
 //Array the contains the data need for the catalog
 //Preferably order this alphabetically
 let catalogData = [
-    new Item("test", "12", "14", "90", "PS", "56", "LO", "14")
+    new Item("test", "12", "14", ["90"], "PS", "56", "LO", "14"),
+    new Item("Stardew Valley", " You've inherited your grandfather's old farm plot in Stardew Valley. Armed with hand-me-down tools and a few coins, you set out to begin your new life. Can you learn to live off the land and turn these overgrown fields into a thriving home?", ["Farming Sim", "RPG"], ["GoG", "Steam"], "ConcernedApe", "ConcernedApe", ["2016"], ["modding", "simulation"])
 ]
 
 //Not Finished. DOES NOT WORK!
@@ -69,7 +70,7 @@ let exportData = catalogData;
 
 //Use check data to modify exportData before it is pushed to catalogBuilder
 function checkData() {
-    
+
 }
 
 
