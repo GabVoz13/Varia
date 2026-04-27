44b1a77b0b5dc46209f841499a14cbac95a9d0ad
Matthew Wilpula <mattwilpula@gmail.com>
2026-04-27 13:16:46 -0400

    Pushing catalog update

--- Catalog (WIP)/catalogBuilder.js
+++ Catalog (WIP)/catalogBuilder.js
@@ -2,5 +2,56 @@ import exportData from "./catalogData.js"
 
 document.addEventListener("DOMContentLoaded", function() {
     let container = document.getElementById("catalog");
-    container.innerHTML = exportData[0].getName();
+    let table = document.createElement('table');
+    container.appendChild(table);
+    table.appendChild(document.createElement('tr'));
+    for (let i = 0; i < 8; i++) {
+        table.firstElementChild.appendChild(document.createElement('th'));
+    }
+    table.firstElementChild.childNodes[0].textContent = "Title";
+    table.firstElementChild.childNodes[1].textContent = "Description";
+    table.firstElementChild.childNodes[2].textContent = "Category";
+    table.firstElementChild.childNodes[3].textContent = "Platform";
+    table.firstElementChild.childNodes[4].textContent = "Developer";
+    table.firstElementChild.childNodes[5].textContent = "Publisher";
+    table.firstElementChild.childNodes[6].textContent = "Release Date";
+    table.firstElementChild.childNodes[7].textContent = "Tags";
+    for (let i = 0; i < exportData.length; i++) {
+        let row = document.createElement('tr');
+        
+        table.appendChild(row);
+        for (let j = 0; j < 8; j++) {
+            row.appendChild(document.createElement('td'));
+        }
+        row.childNodes[0].textContent = exportData[i].getName();
+        row.childNodes[1].textContent = exportData[i].getDescription();
+        let categoryData = exportData[i].getCategory();
+        for (let i = 0; i < categoryData.length; i++) {
+            let categoryNode = document.createElement('p');
+            categoryNode.textContent = categoryData[i];
+            row.childNodes[2].appendChild(categoryNode);
+        }
+        let platformData = exportData[i].getPlatform();
+        for (let i = 0; i < categoryData.length; i++) {
+            let platformNode = document.createElement('p');
+            platformNode.textContent = platformData[i];
+            row.childNodes[3].appendChild(platformNode);
+        }
+        row.childNodes[4].textContent = exportData[i].getDeveloper();
+        row.childNodes[5].textContent = exportData[i].getPublisher();
+        let releaseDates = exportData[i].getReleaseDate();
+        for (let i = 0; i < releaseDates.length; i++) {
+            let releaseNode = document.createElement('p');
+            releaseNode.textContent = releaseDates[i];
+            row.childNodes[6].appendChild(releaseNode);
+        }
+        let tagData = exportData[i].getTags();
+        for (let i = 0; i < tagData.length; i++) {
+            row.classList.add(tagData[i]);
+            let tagNode = document.createElement('p');
+            tagNode.textContent = categoryData[i];
+            row.childNodes[7].appendChild(tagNode);
+        }
+    }
+    
 })
--- Catalog (WIP)/catalogData.js
+++ Catalog (WIP)/catalogData.js
@@ -7,32 +7,28 @@ class releaseDate {
     }
 
     getYear() {
-        return year;
+        return this.year;
     }
 
     getMonth() {
-        return month;
+        return this.month;
     }
 
     getDay() {
-        return day;
+        return this.day;
     }
 
     getOther() {
-        return other;
-    }
-
-    getDate() {
-        return fullDate = (year + "/" + month + "/" + day);
+        return this.other;
     }
 }
 
 class Item {
-    constructor(name, description, category, platfrom, developer, publisher, releaseDate, tags) {
+    constructor(name, description, category, platform, developer, publisher, releaseDate, tags) {
         this.name = name; //This should be a String
         this.description = description; //This should be a String
         this.category = category; //This should be an Array; see external taxonomy
-        this.platform = platfrom; //This should be an Array even if it only contains one item.
+        this.platform = platform; //This should be an Array even if it only contains one item.
         this.developer = developer; //This should be a String
         this.publisher = publisher; //This should be a String
         this.releaseDate = releaseDate; //This should be an Array even if it only contains one item.
@@ -42,13 +38,52 @@ class Item {
     getName() {
         return this.name;
     }
+
+    getDescription() {
+        return this.description;
+    }
+
+    getCategory() {
+        return this.category;
+    }
+
+    getDeveloper() {
+        return this.developer;
+    }
+
+    getPublisher() {
+        return this.publisher;
+    }
+
+    getPlatform() {
+        return this.platform;
+    }
+
+    getReleaseDate() {
+        let exportVar = [];
+        for (let i = 0; i < this.releaseDate.length; i++) {
+            if (this.releaseDate[i].getOther() != null) {
+               exportVar[i] = this.releaseDate[i].getOther() + ": " + this.releaseDate[i].getYear() + "/" + this.releaseDate[i].getMonth() + "/" + this.releaseDate[i].getDay();
+            } else if (this.releaseDate[i].getDay() != null) {
+                exportVar[i] = this.releaseDate[i].getYear() + "/" + this.releaseDate[i].getMonth() + "/" + this.releaseDate[i].getDay();
+            } else if (this.releaseDate[i].getMonth() != null) {
+                exportVar[i] = this.releaseDate[i].getMonth() + "/" + this.releaseDate[i].getYear();
+            } else {
+                exportVar[i] = this.releaseDate[i].getYear();
+            }
+        }
+        return exportVar;
+    }
+    getTags() {
+        return this.tags;
+    }
 }
 
 //Array the contains the data need for the catalog
 //Preferably order this alphabetically
 let catalogData = [
-    new Item("test", "12", "14", ["90"], "PS", "56", "LO", "14"),
-    new Item("Stardew Valley", " You've inherited your grandfather's old farm plot in Stardew Valley. Armed with hand-me-down tools and a few coins, you set out to begin your new life. Can you learn to live off the land and turn these overgrown fields into a thriving home?", ["Farming Sim", "RPG"], ["GoG", "Steam"], "ConcernedApe", "ConcernedApe", ["2016"], ["modding", "simulation"])
+    new Item("test", "12", ["14"], ["78"], "PS", "56", [new releaseDate("2025","13","32","OP"), new releaseDate("2049","17","32","HU")], ["1738","1422"]),
+    new Item("Stardew Valley", " You've inherited your grandfather's old farm plot in Stardew Valley. Armed with hand-me-down tools and a few coins, you set out to begin your new life. Can you learn to live off the land and turn these overgrown fields into a thriving home?", ["Farming Sim", "RPG"], ["GoG", "Steam"], "ConcernedApe", "ConcernedApe", [new releaseDate("2016")], ["modding", "simulation"])
 ]
 
 //Not Finished. DOES NOT WORK!
